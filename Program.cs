a4d537cf2cb732a492313667d1cf9976d361d1ea
efrxngg <88632198+efrxngg@users.noreply.github.com>
2023-11-12 00:46:37 +0000

    feat: Implementar un manejar de er excepciones global

--- Program.cs
+++ Program.cs
@@ -16,7 +16,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PruebaContext>(options =>
     options.UseSqlServer(config.GetConnectionString("cn")));
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ManejadorExcepciones>();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
